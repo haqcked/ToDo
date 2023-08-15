95dda32ea58f9f6df8537dda1bfbf1e1498410ad
abdulhaq abdullah <haqabdullahov@gmail.com>
2023-08-15 16:00:45 +0600

    added bg image, removed bootstrap classes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const search = document.querySelector('.search input')
 
 const createTodo = todo => {
   const html = `
-  <li class="list-group-item d-flex justify-content-between align-items-center">
+  <li>
     <span>${todo}</span>
     <i class="far fa-trash-alt delete"></i>
   </li>
